31dca4e2d401df36d28fdb6e038fb0989345c663
BimorVim <bimbimbim.niconico@gmail.com>
2018-05-09 15:00:02 +0900

    Docコメントの日本語化

--- ConsoleControle.java
+++ ConsoleControle.java
@@ -3,12 +3,21 @@ import java.io.IOException;
 public class ConsoleControle {
 	private ProcessBuilder pb;
 
-	// constructor
+	/**
+	 * ConsoleControleクラスのコンストラクタです。
+	 * 引数に指定した外部コマンドを実行させる環境を構築します。
+	 * 実際に実行させるにはConsoleControle#execute()を使用します。
+	 * @param command 実行させたい外部コマンド
+	 */
 	public ConsoleControle(String... command) {
 		pb = new ProcessBuilder(command);
 	}
 
-	// execute the command
+	/**
+	 * コンストラクタで指定された外部コマンドを実行するメソッドです。
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
 	public void execute() throws IOException, InterruptedException {
 		pb.inheritIO().start().waitFor();
 	}
